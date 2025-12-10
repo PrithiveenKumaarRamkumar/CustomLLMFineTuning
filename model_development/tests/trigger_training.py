ac653c40849c15198ab3cdf63cf5aec32088cd45
Pranudeep Metuku <pranudeepmetuku10@gmail.com>
2025-12-09 23:14:06 -0500

    Add project files without secrets

--- model_development/tests/trigger_training.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Trigger Airflow DAG
-response = requests.post(
-    "http://localhost:8080/api/v1/dags/starcoder2_training_pipeline/dagRuns",
-    json={},
-    auth=("admin", "admin")
-)
-print(response.json())
